ce28a1c492a32f8844e45a37f36a5a623fc1904f
sm4ck3d <sm4cked@gmail.com>
2024-01-09 13:25:53 +0000

    bot works

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -46,6 +46,8 @@ to quickly create a Cobra application.`,
 			switch payload {
 			case "hello":
 				err = m.Send(fmt.Sprintf("Hello,I'm Kbot %s!", appVersion))
+			case "bye":
+				err = m.Send(fmt.Sprintf("Bye-bye!"))
 
 			}
 			return err
